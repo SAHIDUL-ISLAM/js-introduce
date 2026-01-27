43c09164b5f79379d2e621845da019a54986c08c
sahidul-islam <sahidul99622@gmail.com>
2026-01-27 11:30:43 +0600

    modify nan defination

--- nan.js
+++ nan.js
@@ -1,9 +1,11 @@
 
 let a = 4-"hello";
-console.log(a);
+console.log(a); // nan because 4 is a number but hello is a string
 console.log(typeof a);
+console.log(isNaN(a)); //check a variable, it is number or not?
 
 // **
 let abc = NaN;
 console.log(abc)
 console.log(typeof abc);
+console.log(isNaN(abc)); //true, because nan is a number type.
